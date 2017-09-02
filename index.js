22658f5e8ec3611b523c9ebac31247f85f730621
Alon Kaduri <alonk@wix.com>
2017-09-02 13:04:04 +0300

    alonkad

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var app = express();
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+app.use(allowCrossDomain);
+
+app.get('*', function(req, res) {
+    res.json({
+        success: true,
+        request: {
+            header: req.headers,
+            query: req.query,
+            url: req.url,
+            ip: req.ip
+        }
+    });
+});
+
+/* Start the server */
+var port = process.env.PORT || 8006;
+app.listen(port, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
